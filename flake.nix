b846615484ab44571e1f275ebe4ba4846ff45563
Philip Taron <philip.taron@gmail.com>
2026-01-07 16:36:23 -0800

    Golf the flake further to use callPackage

--- flake.nix
+++ flake.nix
@@ -5,32 +5,30 @@
   inputs.systems.url = "github:nix-systems/default";
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      systems,
-    }:
+    inputs:
     let
-      eachSystem = nixpkgs.lib.genAttrs (import systems);
+      eachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
+      drv =
+        {
+          stdenv,
+          curl,
+          cjson,
+          pkg-config,
+        }:
+        stdenv.mkDerivation {
+          name = "h";
+          src = ./.;
+          buildInputs = [
+            curl
+            cjson
+          ];
+          nativeBuildInputs = [ pkg-config ];
+          makeFlags = [ "PREFIX=$(out)" ];
+        };
     in
     {
-      packages = eachSystem (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.stdenv.mkDerivation {
-            name = "h";
-            src = ./.;
-            buildInputs = [
-              pkgs.curl
-              pkgs.cjson
-            ];
-            nativeBuildInputs = [ pkgs.pkg-config ];
-            makeFlags = [ "PREFIX=$(out)" ];
-          };
-        }
-      );
+      packages = eachSystem (system: {
+        default = inputs.nixpkgs.legacyPackages.${system}.callPackage drv { };
+      });
     };
 }

19e1c60fc02894c09d68dd0f5e5ab43c578a2b0e
Philip Taron <philip.taron@gmail.com>
2026-01-07 16:32:32 -0800

    Format flake.nix with nixfmt

--- flake.nix
+++ flake.nix
@@ -4,20 +4,33 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.systems.url = "github:nix-systems/default";
 
-  outputs = { self, nixpkgs, systems }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
     let
       eachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      packages = eachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in {
+      packages = eachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
           default = pkgs.stdenv.mkDerivation {
             name = "h";
             src = ./.;
-            buildInputs = [ pkgs.curl pkgs.cjson ];
+            buildInputs = [
+              pkgs.curl
+              pkgs.cjson
+            ];
             nativeBuildInputs = [ pkgs.pkg-config ];
             makeFlags = [ "PREFIX=$(out)" ];
           };
-        });
+        }
+      );
     };
 }
